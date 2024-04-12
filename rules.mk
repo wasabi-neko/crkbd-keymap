79aa7fbcb715c10e3823536eddc1eb7b1e86808a
AnJung wasabi-neko <wasabi.neko.owo@gmail.com>
2024-04-12 15:52:31 +0800

    Change OLED driver
    Note: Not sure what oled driver should i use after qmk updated, So i
    just commented it.

--- rules.mk
+++ rules.mk
@@ -1,7 +1,7 @@
 MOUSEKEY_ENABLE = yes    # Mouse keys
 RGBLIGHT_ENABLE = no # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+# OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
